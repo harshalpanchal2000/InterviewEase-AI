542e21443f6e3de8798de6da3002659975a2fe6a
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-03-30 00:03:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,3 @@
-import streamlit as st
-import random
-import csv
-
-# Define question pools for each level
-junior_questions = [
-    "What is the Central Limit Theorem?",
-    "How can outlier values be treated?",
-    "Explain the concept of overfitting in machine learning.",
-    "Explain confidence intervals.",
-    "What is the difference between supervised and unsupervised learning?"
-]
-
-mid_level_questions = [
-    "What is regularization in machine learning?",
-    "Explain the bias-variance tradeoff.",
-    "What are the assumptions of linear regression?",
-    "What is the significance of p-value?",
-    "You are given a data set consisting of variables with more than 30 percent missing values. How will you deal with them?"
-]
-
-senior_questions = [
-    "What is deep learning and how does it differ from traditional machine learning?",
-    "What are some common techniques for dimensionality reduction?",
-    "What are the feature selection methods used to select the right variables?",
-    "Facebook composer, the posting tool, dropped from 3% posts per user last month to 2.5% posts per user today. How would you investigate what happened?",
-    "If the labels are known in a clustering project, how would you evaluate the performance of the model?"
-]
-
 def main():
     st.sidebar.title("Interview Ease AI")
     st.sidebar.write("Welcome to Interview Ease AI, your ultimate solution for data scientist hiring! With customizable interview templates and advanced NLP analysis, streamline your hiring process effortlessly. Say hello to a new era of interviews.")
@@ -51,16 +22,17 @@ def main():
 def display_question(questions, session_state):
     st.header("Interview Questions")
     current_question = questions[session_state["question_index"]]
-    #st.subheader(f"Question {session_state['question_index'] + 1}")
     st.write(current_question)
     answer = st.text_area("Your Answer:", value="")
     
     if st.button("Submit"):
-        st.subheader(f"Question {session_state['question_index'] + 1}")
         session_state["answers"].append(answer)
         session_state["question_index"] += 1
-        if session_state["question_index"] < 4:
+        if session_state["question_index"] < len(questions):
             st.experimental_rerun()
+
+    if session_state["question_index"] == len(questions):
+        display_responses(session_state["answers"])
 
 def display_responses(answers):
     st.header("Your Responses")
