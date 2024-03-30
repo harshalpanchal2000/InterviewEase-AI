b82d92ba1c641c0eb125395172b40f030ab9c5a2
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-03-30 00:14:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,31 @@ import streamlit as st
 import csv
 import random 
 
+# Define question pools for each level
+junior_questions = [
+    "What is the Central Limit Theorem?",
+    "How can outlier values be treated?",
+    "Explain the concept of overfitting in machine learning.",
+    "Explain confidence intervals.",
+    "What is the difference between supervised and unsupervised learning?"
+]
+
+mid_level_questions = [
+    "What is regularization in machine learning?",
+    "Explain the bias-variance tradeoff.",
+    "What are the assumptions of linear regression?",
+    "What is the significance of p-value?",
+    "You are given a data set consisting of variables with more than 30 percent missing values. How will you deal with them?"
+]
+
+senior_questions = [
+    "What is deep learning and how does it differ from traditional machine learning?",
+    "What are some common techniques for dimensionality reduction?",
+    "What are the feature selection methods used to select the right variables?",
+    "Facebook composer, the posting tool, dropped from 3% posts per user last month to 2.5% posts per user today. How would you investigate what happened?",
+    "If the labels are known in a clustering project, how would you evaluate the performance of the model?"
+]
+
 def main():
     st.sidebar.title("Interview Ease AI")
     st.sidebar.write("Welcome to Interview Ease AI, your ultimate solution for data scientist hiring! With customizable interview templates and advanced NLP analysis, streamline your hiring process effortlessly. Say hello to a new era of interviews.")
